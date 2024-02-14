c1db91158155bcf6325547caf5a4790e2c04c1b0
bhanuja2000 <127472975+bhanuja2000@users.noreply.github.com>
2024-02-14 18:50:33 +0530

    profile page build start

--- lib/profile.dart
+++ lib/profile.dart
@@ -11,18 +11,80 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(actions: [
-        PopupMenuButton(
-            itemBuilder: (BuildContext context) => [
-                  const PopupMenuItem(
-                      value: "first item", child: Text("Traval Hestory")),
-                  const PopupMenuItem(
-                      value: "first item", child: Text("preferred contacts")),
-                  const PopupMenuItem(
-                      value: "first item", child: Text("Main page")),
-                  const PopupMenuItem(value: "first item", child: Text("Help"))
-                ])
-      ]),
+      appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 67, 166, 248),
+          actions: [
+            PopupMenuButton(
+                itemBuilder: (BuildContext context) => [
+                      PopupMenuItem(
+                        value: "first item",
+                        child: const Text("Traval Hestory"),
+                        onTap: () => print("Traval Hestory"),
+                      ),
+                      PopupMenuItem(
+                        value: "second item",
+                        child: const Text("preferred contacts"),
+                        onTap: () => print("preferred contacts"),
+                      ),
+                      PopupMenuItem(
+                        value: "third item",
+                        child: const Text("Main page"),
+                        onTap: () => print("Main page"),
+                      ),
+                      PopupMenuItem(
+                        value: "first item",
+                        child: const Text("Help"),
+                        onTap: () {
+                          print("help");
+                        },
+                      )
+                    ])
+          ]),
+      body: Column(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(30),
+                  bottomRight: Radius.circular(30),
+                ),
+                color: Color.fromARGB(255, 67, 166, 248)),
+            child: Column(
+              children: [
+                Center(
+                  child: Container(
+                    height: 150,
+                    width: 150,
+                    //margin: const EdgeInsets.all(40),
+                    decoration: const BoxDecoration(
+                        shape: BoxShape.circle, color: Colors.grey),
+                    child: ClipOval(
+                        child: Image.asset(
+                      "assets/user.png",
+                      fit: BoxFit.cover,
+                      height: 250,
+                      width: 250,
+                    )),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(bottom: 20),
+                  child: const Text(
+                    "Bhanuja",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
