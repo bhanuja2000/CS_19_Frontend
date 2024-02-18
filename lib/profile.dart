7c8c62900807c5cbb1a87dd3dc19fad58b43c50e
bhanuja2000 <127472975+bhanuja2000@users.noreply.github.com>
2024-02-18 12:17:09 +0530

    profile done

--- lib/profile.dart
+++ lib/profile.dart
@@ -41,14 +41,16 @@ class _ProfileState extends State<Profile> {
                     ])
           ]),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
             decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(30),
-                  bottomRight: Radius.circular(30),
-                ),
-                color: Color.fromARGB(255, 67, 166, 248)),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30),
+              ),
+              color: Color.fromARGB(255, 67, 166, 248),
+            ),
             child: Column(
               children: [
                 Center(
@@ -81,6 +83,136 @@ class _ProfileState extends State<Profile> {
                   ),
                 )
               ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 10, left: 15),
+            child: const Row(
+              children: [
+                Text(
+                  "Profile",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 20, left: 10),
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  "Name",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 10, left: 10),
+            child: const Text(
+              "M.D.B.R Kannangara",
+              style: TextStyle(fontSize: 15),
+            ),
+          ),
+          Divider(
+            thickness: 3,
+            color: Colors.grey,
+            indent: 10,
+            endIndent: 10,
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 20, left: 10),
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  "Email",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 10, left: 10),
+            child: const Text(
+              "bsathsara@gmail.com",
+              style: TextStyle(fontSize: 15),
+            ),
+          ),
+          const Divider(
+            thickness: 3,
+            color: Colors.grey,
+            indent: 10,
+            endIndent: 10,
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 20, left: 10),
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  "Mobile Number",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 10, left: 10),
+            child: const Text(
+              "0754806795",
+              style: TextStyle(fontSize: 15),
+            ),
+          ),
+          const Divider(
+            thickness: 3,
+            color: Colors.grey,
+            indent: 10,
+            endIndent: 10,
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 20, left: 10),
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  "Account Status",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 10, left: 10),
+            child: const Text(
+              "Premium User",
+              style: TextStyle(fontSize: 15),
+            ),
+          ),
+          const Divider(
+            thickness: 3,
+            color: Colors.grey,
+            indent: 10,
+            endIndent: 10,
+          ),
+          const SizedBox(height: 50),
+          Center(
+            child: SizedBox(
+              width: 200,
+              child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.yellow,
+                      side: BorderSide.none,
+                      shape: const StadiumBorder()),
+                  child: const Text(
+                    "Log Out",
+                    style: TextStyle(color: Colors.black),
+                  )),
             ),
           )
         ],
