e5bf01c823c989300a60a7946e8567d0191dee0b
bhanuja2000 <127472975+bhanuja2000@users.noreply.github.com>
2024-03-19 16:46:07 +0530

    reset page complete

--- lib/passwordreset.dart
+++ lib/passwordreset.dart
@@ -2,28 +2,32 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:textfild/profile.dart';
 
 class PasswordResetPage extends StatelessWidget {
   TextEditingController password = TextEditingController();
-  fetchpassword() async {
-    var url = Uri.http('localhost:8080', '/api/findemail');
-    try {} catch (e) {
+  String jwtToken =
+      "eyJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJCaGFudWphIiwic3ViIjoiYnNhdGhzYXJhQGdtYWlsLmNvbSIsImV4cCI6MTcxMDg0Nzc3NSwiaWF0IjoxNzEwODQ2ODc1LCJzY29wZSI6IlJFQUQifQ.epdCPZHRsSELcjh-sgx_WHH0djOh0Z_9bJXab3VkyJxH1Hp2EeH7JOsPLvkM_xzOKn6g7msSR8aWo7Gh-RfnMz_CoUq3R-x6BH8lSQpvMIvWMJDUPLXuGP0kRSG2kH2mfSB2mrKMp1ZWnXjUWi1UUajz6xuTBHGSZ6xAc-49UXRRe_K8tc4q9ngEu-dYBuDthlfQvj_JQelWEV76GqVDfaiGiJ9Jd23-X96lywrYv5DiBRhQMwmI1HTEV9FF0xMHQf70kzhb_f07jwFtQ5Cd-fPBCCa2eiYdR-KVItJQ76K2k6iwgSMv45hyw7fwFfdK3kfwU9jPh1xR6OmFY9Pyow";
+  void fetchpassword(String pass, BuildContext context) async {
+    try {
+      var url = Uri.http('localhost:8080', '/api/passwordreset');
       var response = await http.post(
         url,
         headers: {
           'Content-Type': 'application/json',
           'Authorization': 'Bearer $jwtToken',
         },
-        body: jsonEncode({"password": "$password"}),
+        body: jsonEncode({"email": "bsathsara@gmail.com", "password": pass}),
       );
-      if (response.body == "okay") {
-        print("password reaet done");
-      }
-      else{
-        print("not done")
-      }
 
-      print(e.toString());
+      if (response.statusCode == 200) {
+        print("Successfully update" + response.body);
+        showPasswordUpdateSuccess(context);
+      } else {
+        print("Registration failed with status code: ${response.statusCode}");
+      }
+    } catch (e) {
+      print("Error during registration: $e");
     }
   }
 
@@ -56,7 +60,8 @@ class PasswordResetPage extends StatelessWidget {
             const SizedBox(height: 20.0),
             ElevatedButton(
               onPressed: () {
-                print(password.text.toString());
+                fetchpassword(password.text.toString(), context);
+                print(password.text);
               },
               child: Text('Reset Password'),
             ),
@@ -64,5 +69,20 @@ class PasswordResetPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void showPasswordUpdateSuccess(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Password updated successfully!'),
+        duration: Duration(seconds: 2), // Adjust duration as needed
+      ),
+    );
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const Profile()),
+      );
+    });
   }
 }
